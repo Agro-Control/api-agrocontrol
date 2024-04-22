7776889df07060f44f3c27d2a6cd7e0369083285
Vinicius Antunes <vinie.antuness@gmail.com>
2024-04-22 13:12:37 -0300

    add cors, e mudando ids talhao (mudança pessoal do front)

--- app.py
+++ app.py
@@ -15,8 +15,16 @@ from model.empresa_model import Empresa
 from service.empresa_service import EmpresaService
 from model.usuario_model import Usuario
 from service.usuario_service import UsuarioService
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 @app.exception_handler(Exception)
 async def generic_exception_handler(request, exc):
