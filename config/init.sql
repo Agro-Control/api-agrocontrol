2a4580a63ae7d4d72248a101a1ba185af7ada309
TrivialX <Gabriel.Yan.Russo@gmail.com>
2024-05-01 19:47:58 -0300

    Change SQL

--- config/init.sql
+++ config/init.sql
@@ -1,17 +1,26 @@
 CREATE TABLE Usuario (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100),
-    email VARCHAR(255),
     cpf VARCHAR(11),
     telefone VARCHAR(14),
-    status CHAR DEFAULT 'A',
+    turno CHAR,
+    email VARCHAR(255),
+    matricula VARCHAR(20),
+    senha VARCHAR(255),
     data_contratacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    tipo CHAR(1),
+    status CHAR DEFAULT 'A' COMMENT 'TABELA STATUS: A = ATIVO, I = INATIVO',
     gestor_id INT,
     empresa_id INT,
-    matricula VARCHAR(20),
-    turno VARCHAR(5),
-    tipo CHAR(1),
-    senha VARCHAR(255)
+    unidade_id INT,
+    grupo_id INT
+);
+
+CREATE TABLE Grupo (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+    status CHAR DEFAULT 'A' COMMENT 'TABELA STATUS: A = ATIVO, I = INATIVO',
+    data_cricao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Empresa (
@@ -26,19 +35,14 @@ CREATE TABLE Empresa (
     logradouro VARCHAR(60),
     numero VARCHAR(20),
     complemento VARCHAR(60),
-    status CHAR DEFAULT 'A',
+    status CHAR DEFAULT 'A' COMMENT 'TABELA STATUS: A = ATIVO, I = INATIVO',
 	data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    telefone_responsavel VARCHAR(11),
-    email_responsavel VARCHAR(60),
-    nome_responsavel VARCHAR(60),
-    gestor_id INT,
-    grupo_empresarial_id INT
+    grupo_id INT
 );
 
 CREATE TABLE Unidade(
 	id SERIAL PRIMARY KEY,
     nome VARCHAR(60),
-    cnpj VARCHAR(14),
     cep VARCHAR(8),
     estado VARCHAR(20),
     cidade VARCHAR(40),
@@ -46,19 +50,18 @@ CREATE TABLE Unidade(
     logradouro VARCHAR(60),
     numero VARCHAR(20),
     complemento VARCHAR(60),
-    status CHAR DEFAULT 'A',
+    status CHAR DEFAULT 'A' COMMENT 'TABELA STATUS: A = ATIVO, I = INATIVO',
 	data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     empresa_id INT,
     gestor_id INT
-);
+    );
 
 CREATE TABLE Talhao(
 	id SERIAL PRIMARY KEY,
 	codigo VARCHAR(10),
 	tamanho VARCHAR(5),
-	status CHAR DEFAULT 'A',
-	gestor_id INT,
-	empresa_id INT
+	status CHAR DEFAULT 'A' COMMENT 'TABELA STATUS: A = ATIVO, I = INATIVO',
+	unidade_id INT
 );
 
 CREATE TABLE Maquina(
@@ -66,11 +69,11 @@ CREATE TABLE Maquina(
 	nome VARCHAR(30), 
 	fabricante VARCHAR(50), 
 	modelo VARCHAR(20),
-	status CHAR DEFAULT 'A',
+	status CHAR DEFAULT 'A' COMMENT 'TABELA STATUS: A = ATIVO, I = INATIVO, M = MANUTENCAO',
 	capacidade_operacional INT,
 	data_aquisicao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	gestor_id INT,
-	empresa_id INT
+	unidade_id INT
+
 );
 
 CREATE TABLE Ordem_Servico(
@@ -78,90 +81,92 @@ CREATE TABLE Ordem_Servico(
 	data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	data_inicio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	data_fim TIMESTAMP DEFAULT NULL,
-	status CHAR DEFAULT 'A',
+	status CHAR DEFAULT 'A' COMMENT 'TABELA STATUS: A = ATIVO, E = EXECUTANDO, F = FINALIZADO, C = CANCELADO',
 	velocidade_minima double precision,
 	velocidade_maxima double precision,
 	rpm double precision,
-	id_gestor INT,
-	id_unidade INT,
-	id_empresa INT,
-	id_talhao INT,
-	id_maquina INT
+	gestor_id INT,
+	unidade_id INT,
+	empresa_id INT,
+	talhao_id INT,
+	maquina_id INT
 );
 
 CREATE TABLE Ordem_Servico_Operador(
-	id_ordem_servico INT,
-	id_operador INT
+	ordem_servico_id INT,
+	operador_id INT
 );
 
 ALTER TABLE Usuario
-ADD CONSTRAINT fk_gestor_gestor_criacao_id FOREIGN KEY (gestor_id) REFERENCES Usuario(id),
-ADD CONSTRAINT fk_gestor_empresa_id FOREIGN KEY (empresa_id) REFERENCES Empresa(id);
+ADD CONSTRAINT fk_usuario_gestor_criacao_id FOREIGN KEY (gestor_id) REFERENCES Usuario(id),
+ADD CONSTRAINT fk_usuario_empresa_id FOREIGN KEY (empresa_id) REFERENCES Empresa(id),
+ADD CONSTRAINT fk_usuario_unidade_id FOREIGN KEY (unidade_id) REFERENCES Unidade(id),
+ADD CONSTRAINT fk_usuario_grupo_id FOREIGN KEY (grupo_id) REFERENCES Grupo(id);
 
 ALTER TABLE Empresa
-ADD CONSTRAINT fk_empresa_gestor_criacao_id FOREIGN KEY (gestor_id) REFERENCES Usuario(id);
+ADD CONSTRAINT fk_empresa_grupo_id FOREIGN KEY (grupo_id) REFERENCES Grupo(id);
 
 ALTER TABLE Unidade
 ADD CONSTRAINT fk_unidade_empresa_id FOREIGN KEY (empresa_id) REFERENCES Empresa(id),
 ADD CONSTRAINT fk_unidade_gestor_criacao_id FOREIGN KEY (gestor_id) REFERENCES Usuario(id);
 
 ALTER TABLE Talhao
-ADD CONSTRAINT fk_talhao_gestor_id FOREIGN KEY (gestor_id) REFERENCES Usuario(id),
-ADD CONSTRAINT fk_talhao_empresa_id FOREIGN KEY (empresa_id) REFERENCES Empresa(id);
+ADD CONSTRAINT fk_talhao_unidade_id FOREIGN KEY (unidade_id) REFERENCES Unidade(id);
 
 ALTER TABLE Maquina
-ADD CONSTRAINT fk_maquina_gestor_id FOREIGN KEY (gestor_id) REFERENCES Usuario(id),
-ADD CONSTRAINT fk_maquina_empresa_id FOREIGN KEY (empresa_id) REFERENCES Empresa(id);
+ADD CONSTRAINT fk_maquina_unidade_id FOREIGN KEY (unidade_id) REFERENCES Unidade(id);
 
 ALTER TABLE Ordem_Servico
-ADD CONSTRAINT fk_ordem_servico_gestor_id FOREIGN KEY (id_gestor) REFERENCES Usuario(id),
-ADD CONSTRAINT fk_ordem_servico_unidade_id FOREIGN KEY (id_unidade) REFERENCES Unidade(id),
-ADD CONSTRAINT fk_ordem_servico_empresa_id FOREIGN KEY (id_empresa) REFERENCES Empresa(id),
-ADD CONSTRAINT fk_ordem_servico_talhao_id FOREIGN KEY (id_talhao) REFERENCES Talhao(id),
-ADD CONSTRAINT fk_ordem_servico_maquina_id FOREIGN KEY (id_maquina) REFERENCES Maquina(id);
+ADD CONSTRAINT fk_ordem_servico_gestor_id FOREIGN KEY (gestor_id) REFERENCES Usuario(id),
+ADD CONSTRAINT fk_ordem_servico_unidade_id FOREIGN KEY (unidade_id) REFERENCES Unidade(id),
+ADD CONSTRAINT fk_ordem_servico_empresa_id FOREIGN KEY (empresa_id) REFERENCES Empresa(id),
+ADD CONSTRAINT fk_ordem_servico_talhao_id FOREIGN KEY (talhao_id) REFERENCES Talhao(id),
+ADD CONSTRAINT fk_ordem_servico_maquina_id FOREIGN KEY (maquina_id) REFERENCES Maquina(id);
 
 ALTER TABLE Ordem_Servico_Operador
-ADD CONSTRAINT fk_ordem_servico_operador_ordem_id FOREIGN KEY (id_ordem_servico) REFERENCES Ordem_Servico(id),
-ADD CONSTRAINT fk_ordem_servico_operador_id FOREIGN KEY (id_operador) REFERENCES Usuario(id);
-
---	Inserindo dados basicos	--
+ADD CONSTRAINT fk_ordem_servico_operador_ordem_id FOREIGN KEY (ordem_servico_id) REFERENCES Ordem_Servico(id),
+ADD CONSTRAINT fk_ordem_servico_operador_id FOREIGN KEY (operador_id) REFERENCES Usuario(id);
 
 
--- Inserir um admin pela saco
-INSERT INTO Usuario (nome, status, tipo )
-VALUES ('21024436047', 'A', 'A');
+-- Inserir um admin pela saco (versão final é o unico gp que deve existir)
+INSERT INTO Grupo (nome)
+VALUES ('Prime Group'), ('Second Group');
+
+-- Inserir um admin pela saco (versão final é o unico cara que deve existir)
+INSERT INTO Usuario (nome, email, tipo, senha, grupo_id)
+VALUES ('DiretorInicial', 'DiretorInicial@gmail.com', 'A', '4321', 1);
 
 -- Inserir uma empresa
-INSERT INTO Empresa (nome, cnpj, telefone, cep, estado, cidade, bairro, logradouro, telefone_responsavel, email_responsavel, nome_responsavel, gestor_id, grupo_empresarial_id)
-VALUES ('Empresa Exemplo', '74363470000156', '41998989898', '82315150', 'PR', 'Curitiba', 'São Braz', 'Concriz', '41999999999', 'responsavel@example.com', 'Responsável', 1, 1);
+INSERT INTO Empresa (nome, cnpj, telefone, cep, estado, cidade, bairro, logradouro, telefone_responsavel, email_responsavel, nome_responsavel, grupo_id)
+VALUES ('Empresa1', '74363470000156', '41998989898', '82315150', 'PR', 'Curitiba', 'São Braz', 'Concriz', '41999999999', 'responsavel@example.com', 'Responsável', 1);
 
 -- Inserir duas unidades
-INSERT INTO Unidade (nome, cnpj, cep, estado, cidade, bairro, logradouro, status, empresa_id, gestor_id)
-VALUES ('Unidade 1', '00213983000144', '81170230', 'PR', 'Curitiba', 'Cidade Industrial', 'Cyro Correia Pereira','A', 1, 1),
-       ('Unidade 2', '08292207000199', '81590510', 'PR', 'Curitiba', 'Uberaba', 'Olindo Caetani', 'A', 1, 1);
+INSERT INTO Unidade (nome, cep, estado, cidade, bairro, logradouro, empresa_id, gestor_id)
+VALUES ('Unidade 1', '81170230', 'PR', 'Curitiba', 'Cidade Industrial', 'Cyro Correia Pereira','A', 1, 2),
+       ('Unidade 2', '81590510', 'PR', 'Curitiba', 'Uberaba', 'Olindo Caetani', 1, 2);
 
 -- Inserir um gestor
-INSERT INTO Usuario (cpf, nome, telefone, status, email, tipo, empresa_id, senha)
-VALUES ('21024436047', 'Gestor 1', '999999999', 'A', 'gestor1@example.com', 'G', 1, '12345');
-
-UPDATE Empresa SET gestor_id = (SELECT id FROM Usuario WHERE tipo = 'G') WHERE id = 1;
+INSERT INTO Usuario (cpf, nome, telefone, email, tipo, empresa_id, senha)
+VALUES ('21024436047', 'Gestor 1', '999999999', 'gestor1@example.com', 'G', 1, '12345');
 
 -- Inserir dois operadores
-INSERT INTO Usuario (cpf, matricula, nome, turno, email, status, gestor_id, empresa_id, tipo)
-VALUES ('01590575075', '98989', 'Operador 1', 'Manhã', 'operador1@example.com', 'A', 1, 1, 'O');
+INSERT INTO Usuario (cpf, matricula, nome, turno, gestor_id, unidade_id, tipo)
+VALUES ('01590575075', '98989', 'Operador 1', 'M', 2, 1, 'O'),
+VALUES ('01590575076', '98979', 'Operador 2', 'T', 2, 2, 'O');
 
 -- Inserir uma máquina
 INSERT INTO Maquina (nome, fabricante, modelo, capacidade_operacional, gestor_id, empresa_id)
-VALUES ('Máquina 1', 'Fabricante 1', 'Modelo 1', 100, 1, 1);
+VALUES ('Máquina 1', 'Fabricante 1', 'Modelo 1', 100, 2, 1),
+VALUES ('Máquina 2', 'Fabricante 2', 'Modelo 2', 90, 2, 1);
 
 -- Inserir um talhão
 INSERT INTO Talhao (codigo, tamanho, status, gestor_id, empresa_id)
-VALUES ('1234', '10', 'A', 1, 1);
+VALUES ('1234', '10', 'A', 2, 1);
 
 -- Inserir uma ordem de serviço
-INSERT INTO Ordem_Servico (velocidade_minima, velocidade_maxima, rpm, id_gestor, id_empresa, id_unidade, id_talhao, id_maquina)
-VALUES (10.5, 20.5, 1000.0, 1, 1, 1, 1, 1);
+INSERT INTO Ordem_Servico (velocidade_minima, velocidade_maxima, rpm, gestor_id, empresa_id, unidade_id, talhao_id, maquina_id)
+VALUES (10.5, 20.5, 1000.0, 2, 1, 1, 1, 1);
 
 -- Inserir um talhão
-INSERT INTO Ordem_Servico_Operador (id_ordem_servico, id_operador) 
-VALUES	(1, 1);
+INSERT INTO Ordem_Servico_Operador (ordem_servico_id, operador_id) 
+VALUES	(1, 3);
